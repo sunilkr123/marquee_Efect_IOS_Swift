873c3cb3fb68dfdf6d6d3db69100a6cbae306a3a
sunil kumar <sunilkumarsah6@gmail.com>
2021-06-27 17:40:59 +0530

    pushing to gi

--- textSldingOnLableDemo/ViewController.swift
+++ textSldingOnLableDemo/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var lblSlidingText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fontSize = 50
+        let fontSetting = "<span style=\"font-size: \(fontSize)\"</span>"
+        let marquee = "<html><body><marquee>\(fontSetting)I stand here today humbled by the task before us, grateful for the trust you have bestowed, mindful of the sacrifices borne by our ancestors. I thank President Bush for his service to our nation, as well as the generosity and cooperation he has shown throughout this</marquee></body></html>"
+        webView.loadHTMLString(marquee, baseURL: nil)
+        
     }
-
 
 }
 
